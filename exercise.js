964027e48114b9330a3fa3fa7bf6191af7381818
udinocera <udinocera@gmail.com>
2023-09-09 23:47:59 +0200

    fine

--- exercise.js
+++ exercise.js
@@ -1,1 +1,15 @@
 
+function repeatHello(callback){
+    const interval = setInterval(() => {
+        callback()
+    }, 1000)
+    setTimeout(() => {
+        clearInterval(interval);
+    }, 5000)
+}
+
+const sayHello = () => {
+console.log("Hello");
+}
+
+repeatHello(sayHello)
